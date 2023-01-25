115dc700e60fcdfa87dee2d81c6edd73a295085f
zahidupm <zahidhasanlimon111@gmail.com>
2023-01-25 22:22:48 +0600

    livewire installation

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<!DOCTYPE html>
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,6 +12,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @livewireStyles
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -31,5 +33,7 @@
                 {{ $slot }}
             </main>
         </div>
+
+    @livewireScripts
     </body>
 </html>
